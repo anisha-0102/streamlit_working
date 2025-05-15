b41738aa4b6d2417f7e7236656572d5b48ba7d74
anisha-0102 <anishas0102s@gmail.com>
2025-05-15 12:22:44 +0000

    123

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 import streamlit as st
-st.set_page_config()
+from client import StockApi
+
+
+# Create page title 
+
+st.set_page_config("Stock Market app")
+
+
+# add title
+
+st.title("Stock Market app")
+
+
+# add subheading 
+
+st.subheader("By Anisha Shinde")
