112f29819d12273c9a2b58a3c2ec2108430bb2fd
anisha-0102 <anishas0102s@gmail.com>
2025-05-16 11:38:40 +0000

    1234

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ import streamlit as st
 from client import StockApi
 
 
-# Create page title 
+## Create page title 
 
-st.set_page_config("Stock Market app")
+st.set_page_config(page_title="Stock Market app", layout ="wide")
 
 
-# add title
+## add title for page 
 
 st.title("Stock Market app")
 
 
-# add subheading 
+## add subheading 
 
 st.subheader("By Anisha Shinde")
+
+## Add box for company 
+company = st.text_input("Company Name")
